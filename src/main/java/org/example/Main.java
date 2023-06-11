8ee73fef21fd86f65f038ed7ed141d691103a377
Konstantin Malitskiy <mkonstantinmihai@mail.ru>
2023-06-11 22:06:40 +0300

    foldersize

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String folderPath = "D:/java/Projects";
+        File file = new File(folderPath);
+
+
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder){
+        if(folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for(File file : files){
+            sum+= getFolderSize(file);
+        }
+        return sum;
     }
 }
