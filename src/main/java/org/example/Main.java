f117def1afed2375b84e8b47a9f0640f1d444b02
Konstantin Malitskiy <mkonstantinmihai@mail.ru>
2023-06-12 23:06:27 +0300

    foldersize

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,18 +9,26 @@ public class Main {
         String folderPath = "D:/java/Projects";
         File file = new File(folderPath);
 
+        long start = System.currentTimeMillis();
 
-        System.out.println(getFolderSize(file));
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        //System.out.println(getFolderSize(file));
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
     }
 
-    public static long getFolderSize(File folder){
-        if(folder.isFile()) {
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
             return folder.length();
         }
         long sum = 0;
         File[] files = folder.listFiles();
-        for(File file : files){
-            sum+= getFolderSize(file);
+        for (File file : files) {
+            sum += getFolderSize(file);
         }
         return sum;
     }
